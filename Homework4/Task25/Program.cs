438d4eb3c76def3f3939201c91d396118226fc11
Vladislav Koldunov <kldn61@gmail.com>
2023-10-05 17:16:47 +0300

    Added Homework4

---
+++ Homework4/Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите A = ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите B = ");
+int B = int.Parse(Console.ReadLine()!);
+
+int i =0;
+int result = 1;
+while(i < B)
+{
+    result *= A;
+    i++;
+}
+Console.WriteLine(result);
---
+++ Homework4/Task27/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите a = ");
+int a = int.Parse(Console.ReadLine()!);
+
+// int i = 0;
+// int sum = 0;
+
+int len = Length(a);
+Console.WriteLine($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
+
+int Length(int a)
+{
+int index = 0;
+while (a > 0)
+{
+a /= 10;
+index++;
+}
+return index;
+}
+
+int Sum(int a, int len)
+{
+int sum = 0;
+for (int i = 1; i <= len; i++)
+{
+sum += a % 10;
+a /= 10;
+}
+return sum;
+}
